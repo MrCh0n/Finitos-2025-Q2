04dc1f4d6118b6ff6eab099dd420d0e5d6263502
MrFranco <fiotti@itba.edu.ar>
2026-03-29 22:54:18 -0300

    sigo con main

--- TPs/Tp1_Fem2/main.m
+++ TPs/Tp1_Fem2/main.m
@@ -15,7 +15,7 @@ L = 25; %in
 t = 0.25 ; %in
 tita = 40; %grados
 
-q = 90; %psi
+q = -90; %psi
 
 %% Control
 type = 1; %tipo de elemento: 1 --> "Mindlin" o 2 --> "Degenerado"
@@ -27,17 +27,16 @@ switch type
         T = t;
         dofselem = 6;
         crearK = @crearK_shellMQ4;
+        sym =
     case 2
         T = t*ones(1,4);
         dofselem = 5;
         crearK = @crearK_shell_degeneradoQ4;
+        sym = 
 end
 %% Malla
-bordes = [0,0;
-          L,0;
-          L,L/2;
-          0,L/2];
-[nodos,elems] = mallador_ej1(bordes,div,div,R,tita);
+
+[nodos,elems] = mallador_ej1(L/2,R,tita,div,div);
 
 figure(2)
 draw_Mesh(elems,nodos, 'NodeLabel',true,'Type','Q4','Color','b')
@@ -73,10 +72,69 @@ free = true(ndof,1);
 
 borde_CD = abs(nodos(:,1)) <1e-6; %x = 0
 borde_AC = abs(nodos(:,2)) <1e-6; %y = 0
-%borde_BD = abs(nodos(:,1) - a) <1e-6;
-%borde_AB = abs(nodos(:,2) - a) <1e-6;
+borde_AB = abs(nodos(:,2) - L/2) <1e-6; %y = L/2
 
-% empotrado
-%dofs2fix = [dofs(borde_1,:);dofs(borde_2,:);dofs(borde_3,:);dofs(borde_4,:)];
 
-free(dofs2fix) = false;
+free(dofs(borde_CD,:)) = false; %empotrado
+free(dofs(borde_AC,[1 5 6])) = false; %sym (mov en x, giro en y z)
+free(dofs(borde_AB,[2 4 6])) = false; %sym (mov y, giro en x y z)
+
+%% Cargas
+
+% Definición de las cargas aplicadas
+R = zeros(ndof, 1);
+for i=1:nelem
+    nodoid = elems(i,:);
+    dir = dofs(nodoid,:);
+    dir = reshape(dir', 1, []); %para que sea un vector leyendo primero columnas
+    Ae = area(nodos(nodoid,:));
+    
+
+    R(dir(3:dofselem:4*dofselem)) = R(dir) + Ae*q/4; % Carga en z
+end
+
+
+%% Calculos
+
+Kr = K(free,free);
+
+Rr = R(free);
+
+%Calculo movimientos
+U = zeros(ndof,1);
+U(free) = Kr\Rr;
+
+%% Graficar
+
+%% Funciones
+
+
+function [Ae] = area(nodos)
+    v1 = (nodos(2,:)-nodos(1,:))/norm(nodos(2,:)-nodos(1,:));
+    
+    v3 = cross(v1,(nodos(4,:)-nodos(1,:)));
+    
+    v3 = v3 / norm(v3); % Normalize the cross product vector
+    
+    v2 = cross(v3,v1);
+    
+    Q = [v1;v2;v3]; %v1 es vector fila
+    nodosp = (Q*nodos')'; %nodos locales
+    nodos = nodosp(:,1:2);
+
+    x1 = [-1; 1; 1; -1];
+    y1 = [-1; -1; 1; 1];
+    A = [ones(4,1) x1 y1 x1.*y1];
+    [w, puntos, n] = gauss([2,2]);
+    Ae = 0; % area del elemento
+    for i = 1:n
+        Neta = [0, 1, 0 puntos(i,2)]/A;
+        Nzeta = [0, 0, 1, puntos(i,1)]/A;
+        
+        D = [Neta; Nzeta];
+    
+        J = D*nodos;
+        mult = abs(det(J))*w(i);
+        Ae = Ae + mult;
+    end
+end
--- TPs/Tp1_Fem2/mallador_ej1.m
+++ TPs/Tp1_Fem2/mallador_ej1.m
@@ -1,4 +1,56 @@
-function [nodos, elems] = mallador_ej1(bordes,divx,divy,R,titamax)
+function [nodos, elems] = mallador_ej1(largo,R,titamax,divY,divTita)
+%mallador de elementos Q4 para un cuadrilatero
+%
+%bordes es una matrix de (4,2) de los nodos limitantes al cuadrilatero
+%
+%divx es la cantidad de elementos en la direccion x
+%divy es la cantidad de elementos en la direccion y
+%
+%poner los nodos en bordes como:
+% 4 - - - 3
+% |       |
+% |       |
+% |       |
+% 1 - - - 2 
+    divY = divY + 1;
+    divTita = divTita + 1;
+
+    disY = linspace(0,largo,divY);
+    disTita = linspace(0,titamax,divTita);
+
+    nodos = zeros(divTita*divY,3);
+
+    zmin = R*cosd(titamax);
+    for i = 1:divTita
+        tita = disTita(i);
+        offset = (i-1)*divY;
+        x=R*sind(tita);
+        z = R*cosd(tita) - zmin;
+        for j = 1:divY
+            y = disY(j);
+            nodos(offset+j,:) = [x,y,z];
+        end
+    end
+    
+    nelem = (divY-1)*(divTita-1);
+    elems = zeros(nelem,4);
+    
+    padding = divY;%el mismo de la siguiente columna
+    for i = 1:divTita-1
+        offset = (i-1)*divY;
+        offset_elems = (i-1)*(divY-1);
+        for j = 1:divY-1
+            elems(offset_elems+j,:) = [offset+j offset+j+padding offset+j+padding+1 offset+j+1];
+        end
+    end
+end
+
+function [nodos, elems] = mallador_ej1_(largo,divx,divy,R,titamax)
+bordes = [0,0;
+          R*sind(titamax),0;
+          R*sind(titamax),largo;
+          0,largo];
+
 [nodos, elems] = mallador_cuadrado_Q4(bordes,divx,divy);
 a = max(nodos(:,1));
 
@@ -19,54 +71,3 @@ end
 nodos = aux_nodos;
 end
 
-function [nodos, elems] = mallador_cuadrado_Q4(bordes,divX,divY)
-%mallador de elementos Q4 para un cuadrilatero
-%
-%bordes es una matrix de (4,2) de los nodos limitantes al cuadrilatero
-%
-%divx es la cantidad de elementos en la direccion x
-%divy es la cantidad de elementos en la direccion y
-%
-%poner los nodos en bordes como:
-% 4 - - - 3
-% |       |
-% |       |
-% |       |
-% 1 - - - 2 
-    divX = divX + 1;
-    divY = divY + 1;
-
-    disX = linspace(-1,1,divX);
-    disY = linspace(-1,1,divY);
-
-    nodos = zeros(divX*divY,2);
-
-    x1 = [-1; 1; 1; -1];
-    y1 = [-1; -1; 1; 1];
-    A = [ones(4,1) x1 y1 x1.*y1];
-
-    xi = bordes(:,1);
-    yi = bordes(:,2);
-
-    for i = 1:divX
-        offset = (i-1)*divY;
-        x = disX(i);
-        for j = 1:divY
-            y = disY(j);
-            N = [1 x y x*y]/A;
-            nodos(offset+j,:) = [N*xi,N*yi];
-        end
-    end
-    
-    nelem = (divY-1)*(divX-1);
-    elems = zeros(nelem,4);
-    
-    padding = divY;%el mismo de la siguiente columna
-    for i = 1:divX-1
-        offset = (i-1)*divY;
-        offset_elems = (i-1)*(divY-1);
-        for j = 1:divY-1
-            elems(offset_elems+j,:) = [offset+j offset+j+padding offset+j+padding+1 offset+j+1];
-        end
-    end
-end
